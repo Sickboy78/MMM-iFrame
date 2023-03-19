9696f55c0076bdacddf9df4dcf6376e0503950a1
Sickbart <sickbart@gmx.de>
2023-03-19 12:42:26 +0100

    added timestamp to url

--- MMM-iFrame.js
+++ MMM-iFrame.js
@@ -10,58 +10,63 @@
 Module.register("MMM-iFrame",{
 		// Default module config.
 		defaults: {
-                                frameWidth: "300",
+                frameWidth: "300",
 				width:"100%",
-                                updateInterval: 0.5 * 60 * 1000,
-                                url: ["http://magicmirror.builders/"],
-                                scrolling: "no"
+				updateInterval: 0.5 * 60 * 1000,
+				url: ["http://magicmirror.builders/"],
+				scrolling: "no"
 		},
 
         start: function () {
                 self = this;
-                var count = 0;
+                let count = 0;
                 if (this.config.url.length > 0 ) {
-                      setInterval( function () { 
-                         self.updateDom(1000);
-                         console.log('update' + count++)
-                         }, this.config.updateInterval);
+						setInterval( function () { 
+								self.updateDom(1000);
+								console.log('update' + count++)
+                        }, this.config.updateInterval);
                 }
-	},
-  getRandomInt: function (min, max) {
-    return Math.floor(Math.random() * (max - min)) + min;
-  },
-resume: function() {
-   console.log("Resuming");
-   return this.getDom();
-},
-        getStyles: function() {
-                return [
-                        "MMM-iFrame.css",
-                ];
-        },
+		},
 
-        // Override dom generator.
-	getDom: function() {
-                var { width, height } = this.config;
-                var wrapper = document.createElement("div");
-                
-                wrapper.className = "mmm-iframe"
-                wrapper.style.width = `${this.config.frameWidth}px`;
+		getRandomInt: function (min, max) {
+				return Math.floor(Math.random() * (max - min)) + min;
+		},
 
-                var html = `
-                        <div class="mmm-iframe-wrapper" style="padding-top: ${100 / (width / height)}%;">
-                                <iframe
-                                        src="${this.config.url[this.getRandomInt(0, this.config.url.length)]}"
-                                        width="${width}"
-                                        height="${height}"
-                                        scrolling="${this.config.scrolling}"
-                                ></iframe>
-                        </div>
-                `;
+		resume: function() {
+				console.log("Resuming");
+				return this.getDom();
+		},
 
-                wrapper.insertAdjacentHTML("afterbegin", html);
+		getStyles: function() {
+				return ["MMM-iFrame.css"];
+		},
 
-		return wrapper;
-	}
+		// Override dom generator.
+		getDom: function() {
+				const { width, height } = this.config;
+				const url = this.config.url[this.getRandomInt(0, this.config.url.length)];
+				const hasQueryParams = url.indexOf("?") != -1;
+				const src = url + ( hasQueryParams ? "&timestamp=" : "?timestamp=" ) + Date.now();
+				
+				let wrapper = document.createElement("div");
+						
+				wrapper.className = "mmm-iframe"
+				wrapper.style.width = `${this.config.frameWidth}px`;
+
+				const html = `
+						<div class="mmm-iframe-wrapper" style="padding-top: ${100 / (width / height)}%;">
+								<iframe
+										src="${src}"
+										width="${width}"
+										height="${height}"
+										scrolling="${this.config.scrolling}"
+								></iframe>
+						</div>
+				`;
+
+				wrapper.insertAdjacentHTML("afterbegin", html);
+
+				return wrapper;
+		}
 
 });
